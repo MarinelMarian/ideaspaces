6508436b7646acf6ed63af1ba688cf10386d1353
Alin Pandichi <alin.pandichi@mozaicworks.com>
2016-05-18 15:24:59 +0300

    Transform request to send url encoded form data

--- angular/frontend/app/scripts/services/userService.js
+++ angular/frontend/app/scripts/services/userService.js
@@ -2,12 +2,21 @@
 
 angular.module('sbAdminApp')
   .service('UserService', function($http) {
-    var url = "http://localhost:8080"
+    var url = "http://localhost:8080/user"
+
+    var transformRequest = function(obj) {
+        var str = [];
+        for(var p in obj)
+          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        return str.join("&");
+    };
 
     this.login = function(email, password) {
       return $http({
         method: 'POST',
-        url: url,
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        url: url + "/login",
+        transformRequest: transformRequest,
         data: {
           email: email,
           password: password
@@ -20,7 +29,9 @@ angular.module('sbAdminApp')
     this.signup = function(firstName, lastName, email, password) {
       return $http({
         method: 'POST',
-        url: url + "/registerUser",
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        url: url + "/register",
+        transformRequest: transformRequest,
         data: {
           firstName: firstName,
           lastName: lastName,
