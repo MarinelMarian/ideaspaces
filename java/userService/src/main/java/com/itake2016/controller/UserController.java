19dcfc5ea236a10030bcdf00fc2b294c0cb5a5a6
Alin Pandichi <alin.pandichi@mozaicworks.com>
2016-05-19 14:48:47 +0300

    Implemented register and login backend

--- java/userService/src/main/java/com/itake2016/controller/UserController.java
+++ java/userService/src/main/java/com/itake2016/controller/UserController.java
@@ -2,6 +2,7 @@ package com.itake2016.controller;
 
 import com.itake2016.domain.User;
 import com.itake2016.repository.UserRepository;
+import com.lambdaworks.crypto.SCryptUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +18,21 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 
-
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    @ResponseBody
+    public User login(@RequestParam String email, @RequestParam String password) {
+        User user = userRepository.findByEmail(email);
+        boolean validPassword = SCryptUtil.check(password, user.getPasswordHash());
+        if (validPassword) return user;
+        throw new RuntimeException("Could not perform login");
+    }
 
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	@ResponseBody
-	public void register(@ModelAttribute User user, @RequestParam String password) {
-		userRepository.save(user);
+	public User register(@ModelAttribute User user, @RequestParam String password) {
+        String passwordHash = SCryptUtil.scrypt(password, 16, 16, 16);
+        user.setPasswordHash(passwordHash);
+        userRepository.save(user);
+        return user;
 	}
 }
